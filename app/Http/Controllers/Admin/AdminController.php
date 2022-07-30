2a6eba34db0bb6c21068410446b94c2652823cf5
mouhcine <mouhcine15.00@gmail.com>
2022-07-30 12:25:07 +0200

    first commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // Non è necessaria se si mette il middleware nel web.php
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function dashboard()
+    {
+        $user = Auth::user();
+        // dump($user->toArray());
+        // dump(Auth::check());
+        return view('admin.dashboard');
+    }
+}
